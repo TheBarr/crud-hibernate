456e29afab5d78d08377ec4f3efc2e9f7fabe634
bartek <bartex600700@gmail.com>
2023-11-19 03:25:57 +0100

    Auto create db

--- src/main/java/com/crud/cruddemo/CruddemoApplication.java
+++ src/main/java/com/crud/cruddemo/CruddemoApplication.java
@@ -20,7 +20,7 @@ public class CruddemoApplication {
         return runner -> {
             // createStudent(studentDAO);
 
-            // createMultipleStudents(studentDAO);
+            createMultipleStudents(studentDAO);
 
             // readStudent(studentDAO);
 
@@ -32,7 +32,7 @@ public class CruddemoApplication {
 
             // deleteStudent(studentDAO);
 
-            deleteStudents(studentDAO);
+            // deleteStudents(studentDAO);
         };
     }
 
